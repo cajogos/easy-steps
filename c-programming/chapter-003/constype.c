6e03792927276a4072c806c700a8f9a56ca7cb40
Carlos Ferreira <carlosferreira@outlook.com>
2016-09-07 20:42:16 +0100

    Creating a constant type

---
+++ c-programming/chapter-003/constype.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+	enum SNOOKER { RED = 1, YELLOW, GREEN, BROWN, BLUE, PINK, BLACK };
+
+	enum SNOOKER pair = RED + BLACK;
+	printf("Pair value: %d \n", pair);
+
+	typedef unsigned short int USINT;
+
+	USINT num = 16;
+	printf("Unsigned short int value: %d \n", num);
+
+	return 0;
+}
