a7bcc00251a8d9ba711c8fe8c0ec77118f1402a9
Carlos Ferreira <carlosferreira@outlook.com>
2016-09-18 12:07:23 +0100

    Passing structures to functions

---
+++ c-programming/chapter-009/passstruct.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+typedef struct
+{
+	char *name;
+	int quantity;
+} Item;
+
+Item fruits[3] = { { "Apple", 10 }, { "Orange", 20 }, { "Pear", 30 } };
+
+void display(Item val, Item *ref);
+
+void display(Item val, Item *ref)
+{
+	printf("%s: %d\n", val.name, val.quantity);
+
+	val.name = "Banana";
+	val.quantity = 40;
+	printf("%s: %d\n", val.name, val.quantity);
+
+	printf("%s: %d\n", fruits[0].name, fruits[0].quantity);
+
+	ref->name = "Peach";
+	ref->quantity = 50;
+	printf("%s: %d\n", fruits[0].name, fruits[0].quantity);
+}
+
+int main()
+{
+	display(fruits[0], &fruits[0]);
+
+	return 0;
+}
