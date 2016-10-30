aee349b2d163d9cbaf2003b5c5d60db8d7ce5b55
Carlos Ferreira <carlosferreira@outlook.com>
2016-10-30 13:57:26 +0000

    Creating variables

---
+++ c++-programming/chapter-001/vars.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	char letter; letter = 'A';
+	int number; number = 100;
+	float decimal = 7.5;
+	double pi = 3.14159;
+	bool isTrue = false;
+
+	cout << "char letter: " << letter << endl;
+	cout << "int number: " << number << endl;
+	cout << "float decimal: " << decimal << endl;
+	cout << "double pi: " << pi << endl;
+	cout << "bool isTrue: " << isTrue << endl; 
+
+	return 0;
+}
