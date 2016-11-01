7292216cef2da143f0c956000c0765905c20c3c2
Carlos Ferreira <carlosferreira@outlook.com>
2016-11-01 12:03:27 +0000

    Branching with if

---
+++ c++-programming/chapter-003/ifelse.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int num = 8;
+	char letter = 'A';
+
+	if (num > 5)
+	{
+		cout << "Number exceeds five" << endl;
+	}
+	else
+	{
+		cout << "Number is five or less" << endl;
+	}
+
+	if (letter == 'A')
+	{
+		cout << "Letter is A" << endl;
+	}
+
+	return 0;
+}
