6d9fa47ebcee221118735cef74db1f52afb06eb5
Carlos Ferreira <c@rlos.rocks>
2017-02-04 21:41:31 +0000

    Obtaining user input

---
+++ python/chapter-001/input.py
@@ -0,0 +1,11 @@
+# Initialize a variable with a user-specified value
+user = input('I am Python. What is your name? : ')
+
+# Output a string and a variable value
+print('Welcome', user)
+
+# Initialize another variable with a user-specified value
+lang = input('Favourite programming language? : ')
+
+# Output a string a variable value
+print(lang, 'Is', 'Fun', sep = ' * ', end = '!\n')
