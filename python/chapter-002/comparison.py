bc98549c514cee8f27a22545220971bdeca80e17
Carlos Ferreira <c@rlos.rocks>
2017-02-04 22:56:40 +0000

    Comparing values

---
+++ python/chapter-002/comparison.py
@@ -0,0 +1,16 @@
+nil = 0
+num = 0
+max = 1
+cap = 'A'
+low = 'a'
+
+print('Equality :\t\t', nil, '==', num, nil == num)
+print('Equality :\t\t', cap, '==', low, cap == low)
+
+print('Inequality :\t', nil, '!=', max, nil != max)
+
+print('Greater :\t\t', nil, '>', max, nil > max)
+print('Lesser :\t\t', nil, '<', max, nil < max)
+
+print('More Or Equal :\t', nil, '>=', num, nil >= num)
+print('Less Or Equal :\t', max, '<=', num, max <= num)
