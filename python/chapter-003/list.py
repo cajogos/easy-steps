4a39066707ed4cc60796dbd76da34c170bda14fd
Carlos Ferreira <c@rlos.rocks>
2017-02-04 23:47:46 +0000

    Writing lists

---
+++ python/chapter-003/list.py
@@ -0,0 +1,12 @@
+quarter = ['January', 'February', 'March']
+
+print('First Month :', quarter[0])
+print('Second Month :', quarter[1])
+print('Third Month :', quarter[2])
+
+coords = [[1, 2, 3], [4, 5, 6]]
+
+print('\nTop Left 0,0 :', coords[0][0])
+print('Bottom Right 1,2 :', coords[1][2])
+
+print('\nSecond Month First Letter :', quarter[1][0])
