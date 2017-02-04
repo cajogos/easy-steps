261758209e729171ebc20537289c8dfa6884420c
Carlos Ferreira <c@rlos.rocks>
2017-02-04 21:32:34 +0000

    Employing variables

---
+++ python/chapter-001/var.py
@@ -0,0 +1,15 @@
+# Initialize a variable with an integer value
+var = 8
+print(var)
+
+# Assign a float value to the variable
+var = 3.142
+print(var)
+
+# Assign a string value to the variable
+var = 'Python in easy steps'
+print(var)
+
+# Assign a boolean value to the variable
+var = True
+print(var)
