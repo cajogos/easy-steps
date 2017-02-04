124aef6ce44741e09e52106d38ea75b87ecaf225
Carlos Ferreira <c@rlos.rocks>
2017-02-04 22:50:34 +0000

    Assigning values

---
+++ python/chapter-002/assign.py
@@ -0,0 +1,19 @@
+a = 8
+b = 4
+
+print('Assign:\t\t\t\t', 'a =', a, '\tb =', b)
+
+a += b
+print('Add & Assign:\t\t', 'a =', a, '(8 += 4)')
+
+a -= b
+print('Subtract & Assign:\t', 'a =', a, '(12 - 4)')
+
+a *= b
+print('Multiply & Assign:\t', 'a =', a, '(8 x 4)')
+
+a /= b
+print('Divide & Assign:\t', 'a =', a, '(32 / 4)')
+
+a %= b
+print('Modulus & Assign:\t', 'a =', a, '(8 % 4')
