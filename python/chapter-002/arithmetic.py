94dd17973d712df4d15d81e0f351380c54000405
Carlos Ferreira <c@rlos.rocks>
2017-02-04 22:42:35 +0000

    Doing arithmetic

---
+++ python/chapter-002/arithmetic.py
@@ -0,0 +1,16 @@
+a = 8
+b = 2
+
+print('Addition:\t\t', a, '+', b, '=', a + b)
+
+print('Subtraction:\t', a, '-', b, '=', a - b)
+
+print('Multiplication:\t', a, 'x', b, '=', a * b)
+
+print('Division:\t\t', a, '/', b, '=', a / b)
+
+print('Floor Division:\t', a, '//', b, '=', a // b)
+
+print('Modulus:\t\t', a, '%', b, '=', a % b)
+
+print('Exponent:\t\t ', a, '** = ', a ** b, sep = '')
