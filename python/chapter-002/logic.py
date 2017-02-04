d25a9016558b45ab45292f4915632ee340aa0854
Carlos Ferreira <c@rlos.rocks>
2017-02-04 23:00:10 +0000

    Assessing logic

---
+++ python/chapter-002/logic.py
@@ -0,0 +1,16 @@
+a = True
+b = False
+
+print('AND Logic:')
+print('a and a =', a and a)
+print('a and b =', a and b)
+print('b and b =', b and b)
+
+print('\nOR Logic:')
+print('a or a =', a or a)
+print('a or b =', a or b)
+print('b or b =', b or b)
+
+print('\nNOT Logic:')
+print('a =', a, '\tnot a =', not a)
+print('b =', b, '\tnot b =', not b)
