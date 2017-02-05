06a79621029e7df2582f39f7727d65c722cb27df
Carlos Ferreira <c@rlos.rocks>
2017-02-05 21:06:12 +0000

    Handling Values

---
+++ python/chapter-008/get.py
@@ -0,0 +1,19 @@
+import cgi
+
+data = cgi.FieldStorage()
+
+make = data.getvalue('make')
+model = data.getvalue('model')
+
+print('Content-type: text/html\r\n')
+print('<!DOCTYPE html>')
+print('<html lang="en">')
+print('<head>')
+print('<meta charset="UTF-8">')
+print('<title>Python Response</title>')
+print('</head>')
+print('<body>')
+print('<h1>', make, model, '</h1>')
+print('<a href="get.html">Back</a>')
+print('</body>')
+print('</html>')
