8251c58a31475e32098528259104fe74787db0f8
Carlos Ferreira <c@rlos.rocks>
2017-02-05 15:31:01 +0000

    Formatting Strings

---
+++ python/chapter-006/format.py
@@ -0,0 +1,8 @@
+snack = '{} and {}'.format('Burger', 'Fries')
+print('\nReplaced:', snack)
+
+snack = '{1} and {0}'.format('Burger', 'Fries')
+print('Replaced:', snack)
+
+snack = '%s and %s' % ('Milk', 'Chocolate')
+print('\nSubstituted:', snack)
