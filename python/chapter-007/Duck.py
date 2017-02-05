dcfc7b1d9ec5a9127e744d03b4b1009b656663a0
Carlos Ferreira <c@rlos.rocks>
2017-02-05 18:41:38 +0000

    Harnessing Polymorphism

---
+++ python/chapter-007/Duck.py
@@ -0,0 +1,7 @@
+class Duck:
+
+    def talk(self):
+        print('\nDuck Says: Quack!')
+
+    def coat(self):
+        print('Duck Wears: Feathers')
---
+++ python/chapter-007/Mouse.py
@@ -0,0 +1,7 @@
+class Mouse:
+
+    def talk(self):
+        print('\nMouse Says: Squeak!')
+
+    def coat(self):
+        print('Mouse Wears: Fur')
