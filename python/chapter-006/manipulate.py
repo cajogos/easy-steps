610a354110cf2554ea1ebadc58277ea47cdfcdb1
Carlos Ferreira <c@rlos.rocks>
2017-02-05 15:27:29 +0000

    Manipulating strings

---
+++ python/chapter-006/manipulate.py
@@ -0,0 +1,15 @@
+def display(s):
+    '''Display an argument value.'''
+    print(s)
+
+
+display(display.__doc__)
+
+display(r'C:\Program Files')
+
+display('\nHello' + ' Python')
+
+display('Python In Easy Steps\n'[7:])
+
+display('P' in 'Python')
+display('p' in 'Python')
