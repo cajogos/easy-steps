c7e0ccfb33462cf623251574954cac5f750f82f2
Carlos Ferreira <c@rlos.rocks>
2017-02-05 15:36:55 +0000

    Modifying Strings

---
+++ python/chapter-006/modify.py
@@ -0,0 +1,12 @@
+string = 'Python in easy steps'
+
+print('\nCapitalized:\t', string.capitalize())
+print('\nTitled:\t\t', string.title())
+print('\nCentered:\t', string.center(30, '*'))
+
+print('\nUppercase:\t', string.upper())
+print('\nJoined:\t\t', string.join('**'))
+
+print('\nJustified:\t', string.rjust(30, '*'))
+
+print('\nReplaced:\t', string.replace('s', '*'))
