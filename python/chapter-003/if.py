89dff2cf226b0166b3c12516bbb2e55a54a604de
Carlos Ferreira <c@rlos.rocks>
2017-02-05 00:09:58 +0000

    Branching with if

---
+++ python/chapter-003/if.py
@@ -0,0 +1,14 @@
+num = int(input('Please Enter a Number: '))
+
+if num > 5:
+    print('Number exceeds 5')
+elif num < 5:
+    print('Number is less than 5')
+else:
+    print('Number is 5')
+
+if num > 7 and num < 9:
+    print('Number is 8')
+
+if num == 1 or num == 3:
+    print('Number is 1 or 3')
